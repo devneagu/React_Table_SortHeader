03cc52335e42e412bc0ac7723ae6835e6b814cc2
devneagu <74415486+devneagu@users.noreply.github.com>
2021-06-15 12:31:30 +0300

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,56 @@
 import "./styles.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const TableHeader = styled.th`
+  background: #6f6f6f;
+  padding: 0.5em 0.8em;
+  text-transform: uppercase;
+  cursor: pointer;
+`;
+const TableRow = styled.tr`
+  font-size: 0.8em;
+`;
+const TableData = styled.td`
+  border-bottom: 0.02em solid black;
+  padding: 0.5em;
+  color: #dbd1d0;
+  &.rp {
+    background: #252f38;
+  }
+  &.ri {
+    background: #151e23;
+  }
+`;
+
+const Table = styled.table`
+  text-align: center;
+  width: 100%;
+`;
+
+const InputElement = styled.input`
+  padding: 0.5em;
+  color: black;
+  margin: 1em;
+`;
 
 // https://randomuser.me/api/?results=20
+
+function SearchInput(props) {
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    props.updateSearchData(input);
+  }, [input]);
+  return (
+    <InputElement
+      placeholder="Search"
+      type="text"
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+    />
+  );
+}
 
 function getRandomUser() {
   return axios
@@ -43,19 +91,24 @@ function getObjectKeys(data: any) {
   return listKeys;
 }
 
-function SearchInput(props) {
-  const [input, setInput] = useState("");
-  useEffect(() => {
-    props.updateSearchData(input);
-  }, [input]);
-  return (
-    <input
-      placeholder="search"
-      type="text"
-      value={input}
-      onChange={(e) => setInput(e.target.value)}
-    />
-  );
+function sortColumnRule(ruleColumn) {
+  switch (ruleColumn.direction) {
+    case "ASCENDING":
+      return {
+        column: ruleColumn.column,
+        direction: "DESCENDING"
+      };
+    case "DESCENDING":
+      return {
+        column: ruleColumn.column,
+        direction: "ASCENDING"
+      };
+    default:
+      return {
+        column: ruleColumn.column,
+        direction: "DESCENDING"
+      };
+  }
 }
 
 export default function App() {
@@ -82,56 +135,34 @@ export default function App() {
     });
   }, []);
 
-  function sortColumnRule(ruleColumn) {
-    switch (ruleColumn.direction) {
-      case "ASCENDING":
-        return {
-          column: ruleColumn.column,
-          direction: "DESCENDING"
-        };
-      case "DESCENDING":
-        return {
-          column: ruleColumn.column,
-          direction: "ASCENDING"
-        };
-      default:
-        return {
-          column: ruleColumn.column,
-          direction: "DESCENDING"
-        };
-    }
-  }
-  function compare(a, b) {
-    var column = sortingDirections.column;
-    if (sortingDirections.direction === "ASCENDING") {
-      if (a[column] < b[column]) {
-        return -1;
+  function compareMiddleware(rule) {
+    return function compare(a, b) {
+      var column = rule.column;
+      if (rule.direction === "ASCENDING") {
+        if (a[column] < b[column]) return -1;
+        if (a[column] > b[column]) return 1;
+      } else {
+        if (a[column] > b[column]) return -1;
+        if (a[column] < b[column]) return 1;
       }
-      if (a[column] > b[column]) {
-        return 1;
-      }
-    } else {
-      if (a[column] > b[column]) {
-        return -1;
-      }
-      if (a[column] < b[column]) {
-        return 1;
-      }
-    }
-    return 0;
+      return 0;
+    };
   }
   function sortColumn(column) {
     //{column : "column",direction}
+    const newData = JSON.parse(JSON.stringify(dataSearch));
     if (!sortingDirections.hasOwnProperty("column")) {
-      setSortingDirections({
+      var newRule = {
         column: column,
         direction: "ASCENDING"
-      });
+      };
+      setSortingDirections(newRule);
+      newData.data = newData.data.sort(compareMiddleware(newRule));
     } else {
-      setSortingDirections(sortColumnRule(sortingDirections));
+      const newRule = sortColumnRule(sortingDirections);
+      setSortingDirections(newRule);
+      newData.data = newData.data.sort(compareMiddleware(newRule));
     }
-    const newData = JSON.parse(JSON.stringify(dataSearch));
-    newData.data = newData.data.sort(compare);
 
     setDataSearch(newData);
   }
@@ -139,28 +170,40 @@ export default function App() {
   return (
     <div className="App">
       <SearchInput updateSearchData={updateSearchData} />
-      <table>
-        <thead>
-          <tr>
-            {dataSearch &&
-              dataSearch.headers.map((headerItem, index) => (
-                <th key={index} onClick={() => sortColumn(headerItem)}>
-                  {headerItem}
-                </th>
-              ))}
-          </tr>
-        </thead>
-        <tbody>
-          {dataSearch &&
-            dataSearch.data.map((item, index2) => (
-              <tr key={index2}>
-                {dataSearch.headers.map((headerItem, index) => (
-                  <td key={index}>{item[headerItem]}</td>
+      {dataSearch && dataSearch.data && (
+        <Table border="0" cellSpacing="0" cellPadding="0">
+          <thead>
+            <tr>
+              {dataSearch &&
+                dataSearch.headers.map((headerItem, index) => (
+                  <TableHeader
+                    key={index}
+                    onClick={() => sortColumn(headerItem)}
+                  >
+                    {headerItem}
+                  </TableHeader>
                 ))}
-              </tr>
-            ))}
-        </tbody>
-      </table>
+            </tr>
+          </thead>
+          <tbody>
+            {dataSearch &&
+              dataSearch.data.map((item, index2) => (
+                <TableRow key={index2}>
+                  {dataSearch.headers.map((headerItem, index) => (
+                    <TableData
+                      className={`${index2 % 2 == 0 ? "rp" : "ri"} ${
+                        index % 2 == 0 ? "cp" : "ci"
+                      }`}
+                      key={index}
+                    >
+                      {item[headerItem]}
+                    </TableData>
+                  ))}
+                </TableRow>
+              ))}
+          </tbody>
+        </Table>
+      )}
     </div>
   );
 }
